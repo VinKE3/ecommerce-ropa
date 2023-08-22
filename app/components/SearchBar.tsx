0a70aa9b85969f8f1c5a5cf227faac351c9842f2
kevin munoz <harquin09@gmail.com>
2023-08-22 00:18:51 -0500

    firs

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+
+type Props = {};
+
+const SearchBar = (props: Props) => {
+  return (
+    <div>
+      <div className="flex items-center bg-gray-200 p-2 rounded-full max-md:hidden">
+        <button>
+          <BiSearch size={20} className="opacity-50" />
+        </button>
+        <input
+          type="text"
+          className="outline-none
+          bg-transparent ml-2 caret-blue-500 placeholder:font-light placeholder:text-gray-600 text-[15px]
+          "
+          placeholder="Buscar"
+          autoComplete="false"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
